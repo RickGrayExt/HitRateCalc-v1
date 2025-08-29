2262a96b79b9e7311d2c55167d2d688acdd8a1ae
RickGrayExt <rick.krcarpets@gmail.com>
2025-08-29 09:22:24 +0000

    updated using system fix

--- src/Contracts/StartRunCommand.cs
+++ src/Contracts/StartRunCommand.cs
@@ -1,11 +1,11 @@
-csharpusing System;
+using System;
 
 namespace Contracts
 {
     public record StartRunCommand
     {
         public string DatasetPath { get; init; } = string.Empty;
-        public Guid RunId { get; init; }
+        public Guid RunId { get; init; }           // ← This is line 8
         public int MaxOrdersPerStation { get; init; } = 4;
         public int NumberOfStations { get; init; } = 4;
         public int MaxSkusPerRack { get; init; } = 50;
